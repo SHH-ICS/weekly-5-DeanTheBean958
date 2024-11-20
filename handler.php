fd1d210c76df48074c7e4df00774ad14979dba5c
Dean <dean.pauls@stu.ocsb.ca>
2024-11-20 14:46:38 +0000

    done

--- handler.php
+++ handler.php
@@ -1,23 +1,34 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Welcome Page</title>
+</head>
+
+<body>
+
+  <?php
+  $Temp = "";
+  $result = 0;
+  if (isset($_POST['YourNumber'])) {
+    $Temp = $_POST['YourNumber'];
+  }
+
+  if ($Temp > 0 and $Temp <= 9999999) {
+    while ($Temp >= 0) {
+      $result = $result + ((-1) ** $Temp / (2 * $Temp + 1)) * 4;
+      $Temp = $Temp - 1;
     }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+    echo "<h4>$result</h4>";
+  } else {
+    echo "<h4>Invalid_Number</h4>";
+  }
+
+  ?>
+
+</body>
+
 </html>
