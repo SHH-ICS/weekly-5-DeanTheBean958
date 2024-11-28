ec289fcf8942dc5c5d3ac7ef9062873d05a671f2
Dean <dean.pauls@stu.ocsb.ca>
2024-11-28 00:46:07 +0000

    done

--- assignment.py
+++ assignment.py
@@ -1,11 +1,7 @@
-def calculatePi(digits):
-  result = 0
-  for i in range(digits):
-    print(i)
-    result = result + ((-1)**i/(2*i+1))*4
-  return result
-
 number_of_digits = input(); chars = set('0123456789')
 if all((c in chars) for c in number_of_digits):
   number_of_digits = int(number_of_digits)
-  print(calculatePi(number_of_digits))
+  result = 0 
+  for P in range(number_of_digits):
+    result = result + ((-1)**P/(2*P+1))*4
+  print(result)
