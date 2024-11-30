2106cfc7f91dae332a008ea624cb7f24b64513f4
Dean <dean.pauls@stu.ocsb.ca>
2024-11-30 16:52:27 +0000

    compteted and tested python

--- assignment.py
+++ assignment.py
@@ -1,7 +1,31 @@
-number_of_digits = input(); chars = set('0123456789')
-if all((c in chars) for c in number_of_digits):
-  number_of_digits = int(number_of_digits)
-  result = 0 
-  for P in range(number_of_digits):
-    result = result + ((-1)**P/(2*P+1))*4
-  print(result)
+def calcPi(digits):
+  if digits == "":
+    print("please input a number")
+    return
+  if digits[0] == "-":
+    print("number must be greater than 0")
+    return
+  if not digits.isdigit():
+    print("invaild input")
+    return 
+
+  n = int(digits)
+  if n <= 0:
+    print("number must be greater than 0")
+    return
+  i = 0
+  pi =  0 
+  num = 1
+  den = 1
+
+  while i < n:
+    pi = pi + num/den
+    num = num*-1
+    den = den + 2
+    i = i + 1
+
+  pi = pi*4 
+  print(pi)  
+
+a = input("please enter how many times I should run the loop:")
+calcPi(a)
