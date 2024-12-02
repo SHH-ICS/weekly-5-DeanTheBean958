b6e6e4fd57922fd976e4ab841c587d40ee64cb07
Dean <dean.pauls@stu.ocsb.ca>
2024-12-02 17:23:33 +0000

    reimpleted php and ran all test cases

--- handler.php
+++ handler.php
@@ -5,26 +5,35 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <title>Welcome Page</title>
+  <title>answer Page</title>
 </head>
 
 <body>
 
   <?php
-  $Temp = "";
-  $result = 0;
-  if (isset($_POST['YourNumber'])) {
-    $Temp = $_POST['YourNumber'];
+  
+
+  if (isset($_POST["Number"])) {
+    $Temp = $_POST["Number"];
   }
 
-  if ($Temp > 0 and $Temp <= 9999999) {
-    while ($Temp >= 0) {
-      $result = $result + ((-1) ** $Temp / (2 * $Temp + 1)) * 4;
-      $Temp = $Temp - 1;
+  $N = (int)$Temp;
+  $Result = 0;
+  $Num = 1;
+  $Den = 1;
+  $I = 0;
+
+  if ($N > 0) {
+    while ($I < $N) {
+      $Result = $Result + $Num/$Den;
+      $Num = $Num * -1;
+      $Den = $Den + 2;
+      $I = $I + 1;
     }
-    echo "<h4>$result</h4>";
+    $Result = $Result * 4;
+    echo "<h4>$Result</h4>";
   } else {
-    echo "<h4>Invalid_Number</h4>";
+    echo "<h4>Number must be greater than 0</h4>";
   }
 
   ?>
